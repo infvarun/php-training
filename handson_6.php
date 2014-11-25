a4dabb330e63b9be70e2e76f314800f7c35e946c
Varun <varun06@infosys.com>
2014-11-25 14:34:57 +0530

    Create handson_6.php

---
+++ handson_6.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Write a PHP function to find if number is odd or even.
+* Number should be present in an array.
+* Pass array as parameter to that function.
+* Function Name should be : isOddOrEven($number)
+* For each element echo whether its odd or even.
+* E.g. if array is [9, 2, 3, 8]
+* 9 - Odd
+* 2 - even
+* 3 - Odd
+* 8 - Even
+*/
+
+
+?>
