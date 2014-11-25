eed3e4f20d6cd78443c0c71542d88540ec9f882f
Varun <varun06@infosys.com>
2014-11-25 14:29:59 +0530

    Create handson5.php

---
+++ handson5.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Write a php function to print table for given parameter.
+* Function Name should be : getTable($number)
+* Use for loop for generating table dynamically.
+*/
+
+function getTable($number)
+{
+  //Your Code Here.....
+}
+
+getTable(5);
+
+?>
